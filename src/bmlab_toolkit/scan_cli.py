6c58552523a9fb4e612f17a2bfd07f48d3e72b94
PaulFirs <makloun@hotmail.com>
2025-12-21 20:08:44 +0300

    class

--- src/bmlab_toolkit/scan_cli.py
+++ src/bmlab_toolkit/scan_cli.py
@@ -199,7 +199,7 @@ Examples:
             # USB scan mode
             else:
                 print(f"Scanning for USB JLink programmers...\n")
-                devices = JLinkProgrammer.scan()
+                devices = JLinkProgrammer(log_level=log_level)
             
             if not devices:
                 print("No JLink devices found.")
